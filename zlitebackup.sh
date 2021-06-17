dd634dd331a7d919f02e92e3f7f1bd9b2a6ad964
Christian Zufferey <christian@zufferey.com>
2021-06-17 11:41:50 +0200

    Exclu le dossier .meteor :-(

--- zlitebackup.sh
+++ zlitebackup.sh
@@ -13,7 +13,7 @@
 
 echo -e "
 Système de sauvegarde (backup) économique automatique de Full/Différentiel avec rsync et ssh
-zf 1200711.1704,150209.0838,150625.2241, 161205.1115 200715.1541 210112.0914
+zf 1200711.1704,150209.0838,150625.2241, 161205.1115 200715.1541 210617.1119
 
 Use: ./zlitebackup.sh
 
@@ -49,7 +49,7 @@ TARGET="/Volumes/backupzf1/Backups/macbookprozf"
 EXCLUDE='--exclude=**/Library* --exclude=**/mnt* --exclude=**/.Trash* --exclude=**/.DS_Store* --exclude=**/ansible-deps-cache* '
 EXCLUDE=$EXCLUDE'--exclude=**/.atom* --exclude=**/.git* --exclude=**/.kube* --exclude=**/.cisco* --exclude=**/*.photoslibrary* --exclude=**/VirtualBox?VM?Masters* '
 EXCLUDE=$EXCLUDE'--exclude=**/VirtualBox?VMs* --exclude=**/.vagrant.d/boxes* --exclude=**/.android* --exclude=**/.gradle* --exclude=**/.npm* '
-EXCLUDE=$EXCLUDE'--exclude=**/.cisco*'
+EXCLUDE=$EXCLUDE'--exclude=**/.cisco* --exclude=**/.meteor*'
 
 
 
